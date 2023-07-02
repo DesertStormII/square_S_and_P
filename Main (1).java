ffe0fe53929dba4c7e60836c097cee51c3743984
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2023-07-02 13:05:52 +0500

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    //square
+		int a = 7;
+		int S = a * a;
+		int P = 4 * a;
+		System.out.println(" Perimetr " + P);
+		System.out.println(" Area " + S);
+	}
+}
